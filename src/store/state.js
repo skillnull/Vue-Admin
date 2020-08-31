720018bcc0f2eacdd2e5a52d6036e95a6af5b6c9
skillnull <skillisnull@gmail.com>
2020-08-31 16:35:13 +0800

    update slide nav

--- src/store/state.js
+++ src/store/state.js
@@ -11,38 +11,45 @@ export default {
                     icon: 'dashboard',
                     to: '/dashboard',
                     title: 'Dashboard'
+                },
+                {
+                    icon: 'el-icon-user',
+                    to: '/user',
+                    title: 'User Profile'
+                },
+                {
+                    icon: 'el-icon-date',
+                    to: '/table',
+                    title: 'Table list'
+                },
+                {
+                    icon: 'el-icon-notebook-1',
+                    to: '/typography',
+                    title: 'Icons'
                 }
             ]
         },
         {
-            icon: 'el-icon-user',
-            to: '/user',
-            title: 'User Profile'
-        },
-        {
-            icon: 'el-icon-date',
-            to: '/table',
-            title: 'Table list'
-        },
-        {
-            icon: 'el-icon-notebook-1',
-            to: '/typography',
-            title: 'Icons'
-        },
-        {
             icon: 'el-icon-map-location',
-            to: '/maps',
-            title: 'Maps'
+            to: '',
+            title: 'Others',
+            children: [
+                {
+                    icon: 'el-icon-map-location',
+                    to: '/maps',
+                    title: 'Maps'
+                },
+                {
+                    icon: 'el-icon-upload2',
+                    to: '/upgrade',
+                    title: 'Upgrade to PRO'
+                }
+            ]
         },
         {
             icon: 'el-icon-bell',
             to: '/notifications',
             title: 'Notifications'
-        },
-        {
-            icon: 'el-icon-upload2',
-            to: '/upgrade',
-            title: 'Upgrade to PRO'
         }
     ]
 }
