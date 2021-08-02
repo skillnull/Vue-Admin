25bfdb0ac8738c992387bfbb8950cfeb782e2f89
skillnull <skillisnull@gmail.com>
2021-08-02 15:59:18 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,27 @@
 
 <script>
 export default {
-    name: 'app'
+    name: 'app',
+    created () {
+        if (sessionStorage.getItem("store")) {
+            this.$store.replaceState(Object.assign({}, JSON.parse(sessionStorage.getItem("store"))))
+        }
+
+        window.addEventListener("beforeunload", () => {
+            let cache = []
+            sessionStorage.setItem("store", JSON.stringify(this.$store.state, function (key, value) {
+                if (typeof value === 'object' && value !== null) {
+                    if (cache.indexOf(value) !== -1) {
+                        // Circular reference found, discard key
+                        return;
+                    }
+                    // Store value in our collection
+                    cache.push(value);
+                }
+                return value;
+            }))
+            cache = null
+        })
+    }
 }
 </script>
