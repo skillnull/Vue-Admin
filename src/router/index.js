b827aa76a662e17183f9ba5d46e740ac2d42acf7
skillnull <skillisnull@gmail.com>
2021-11-26 10:30:35 +0800

    feat: router update

--- src/router/index.js
+++ src/router/index.js
@@ -27,4 +27,10 @@ router.beforeEach((to, from, next) => {
     next(true)
 })
 
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+    return originalPush.call(this, location).catch(err => err)
+}
+
 export default router
